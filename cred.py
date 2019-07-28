b9cc9d002ccfb8be41ed62082079957ca0bd6fc2
arkeyve <arkeyve@gmail.com>
2019-07-28 23:08:01 +0530

    init

--- cred.py
+++ cred.py
@@ -1,12 +1,12 @@
 source = {
-    'client_id': r'<source_client_id>',
-    'client_secret': r'<source_client_secret>',
-    'url': r'<source_url>'
+    'client_id': r'3',
+    'client_secret': r'0kDzIuhzlW8qHTZJoScEBD2hgdWJTZahBkM1bL2I',
+    'url': r'http://arkeyve-firefly.herokuapp.com'
     }
 sink = {
-    'client_id': r'<sink_client_id>',
-    'client_secret': r'<sink_client_secret>',
-    'url': r'<sink_url>'
+    'client_id': r'2',
+    'client_secret': r'ewKhUUadfWjBtUuKmRgw4NRY2SpidFBua8Z8KC2Z',
+    'url': r'http://localhost/firefly-iii'
     }
 source['auth_base_url'] = source['url'] + r'/oauth/authorize'
 source['token_url'] = source['url'] + r'/oauth/token'
