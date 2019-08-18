4768ee33185df8f2fba45478f5acd8ebe1cdc491
arkeyve <arkeyve@gmail.com>
2019-08-18 11:56:26 +0530

    import accounts complete, added upload csv function

--- migrate.py
+++ migrate.py
@@ -2,7 +2,9 @@
 from requests_oauthlib import OAuth2Session
 from flask import Flask, request, redirect, session, url_for, render_template
 from flask.json import jsonify
+from werkzeug import secure_filename
 from cred import src, snk
+import json
 import os
 
 # --cred.py
@@ -33,29 +35,12 @@ def set_target(target):
     else:
         return False
 
+data = {"src": {}, "snk": {}}
+
 @app.route('/')
 def index():
-    def get_data(target, type):
-        client = set_target(target)
-        if client:
-            firefly = OAuth2Session(client["client_id"], token=session[target + '_oauth_token'])
-            return firefly.get(client["url"] + '/api/v1/' + type).json()
-        else:
-            return False
-
-    data = None
-    if session and ("src_oauth_token" in session.keys()) and ("snk_oauth_token" in session.keys()):
-        data = {
-            'src': {
-                'accounts': get_data('src', 'accounts')
-            },
-            'snk': {
-                'accounts': get_data('snk', 'accounts')
-            }
-        }
-
     if data:
-        return render_template("index.html", sess=session, data=data)
+        return render_template("index.html", session=session, data=json.dumps(data))
     else:
         return render_template("index.html", sess=session)
 
@@ -70,6 +55,85 @@ def get_connection(target):
         return redirect(authorization_url)
     else:
         return "bad request", 400
+
+@app.route('/get_data/<target>/<type>')
+def get_data(target, type):
+    client = set_target(target)
+    if client:
+        firefly = OAuth2Session(client["client_id"], token=session[target + '_oauth_token'])
+        data[target][type] = firefly.get(client["url"] + '/api/v1/' + type).json()
+        return redirect(url_for('.index'))
+    else:
+        return False
+
+@app.route('/import_accounts')
+def import_accounts():
+    key_map = {
+        "name": ["name", {}],
+        "type": ["type", {
+            "Default account": "asset",
+            "Cash account": "asset",
+            "Asset account": "asset",
+            "Expense account": "expense",
+            "Revenue account": "revenue",
+            "Initial balance account": "asset",
+            "Beneficiary account": "asset",
+            "Import account": "asset",
+            "Loan": "liability",
+            "Reconciliation account": "liability",
+            "Debt": "liability",
+            "Mortgage": "liability"
+        }],
+        "iban": ["iban", {}],
+        "bic": ["bic", {}],
+        "account_number": ["account_number", {}],
+        "opening_balance": ["opening_balance", {}],
+        "opening_balance_date": ["opening_balance_date", {}],
+        "virtual_balance": ["virtual_balance", {}],
+        "currency_id": ["currency_id", {}],
+        "currency_code": ["currency_code", {}],
+        "active": ["active", {}],
+        "include_net_worth": ["include_net_worth", {True: "true", False: "false"}],
+        "account_role": ["role", {}],
+        "credit_card_type": ["credit_card_type", {}],
+        "monthly_payment_date": ["monthly_payment_date", {}],
+        "liability_type": ["liability_type", {}],
+        "liability_amount": ["liability_amount", {}],
+        "liability_start_date": ["liability_start_date", {}],
+        "interest": ["interest", {}],
+        "interest_period": ["interest_period", {}],
+        "notes": ["notes", {}]
+    }
+    if session and ("src_oauth_token" in session.keys()) and ("snk_oauth_token" in session.keys()):
+        firefly = OAuth2Session(snk["client_id"], token=session['snk_oauth_token'])
+        if data and ("accounts" in data["src"].keys()):
+            headers = {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+            for account in data["src"]["accounts"]["data"]:
+                request_body = {}
+                for key in key_map.keys():
+                    if key_map[key][0] in account["attributes"].keys() and account["attributes"][key_map[key][0]] != None:
+                        if len(key_map[key][1]) > 0:
+                            request_body[key] = key_map[key][1][account["attributes"][key_map[key][0]]]
+                        else:
+                            request_body[key] = account["attributes"][key_map[key][0]]
+
+                print(json.dumps(request_body))
+                print(firefly.post(snk["url"] + '/api/v1/accounts', data=json.dumps(request_body), headers=headers).content)
+            print("all accounts imported")
+            return redirect(url_for(".index"))
+        else:
+            return "bad request", 400
+    else:
+        return "bad request", 400
+
+@app.route('/upload_records', methods=["POST"])
+def upload_records():
+    f = request.files['records']
+    f.save('./uploads/' + secure_filename(f.filename))
+    return "upload successful"
 
 @app.route('/callback/<target>', methods=["GET"])
 def callback(target):
--- static/main.js
+++ static/main.js
@@ -3,3 +3,49 @@ var BASE_URL = "http://localhost:5000/";
 function get_conn(target) {
     window.location.replace(BASE_URL + "get_connection/" + target);
 }
+
+function get_data(target, type) {
+    window.location.replace(BASE_URL + "get_data/" + target + "/" + type);
+}
+
+function import_data(type) {
+    window.location.replace(BASE_URL + "import_" + type);
+}
+
+function initiate_uploader() {
+    var form = document.getElementById('file-form');
+    var file = document.getElementById('file-select');
+    var upload_button = document.getElementById('upload-button');
+
+    file.onchange = function(event) {
+        console.log(file.files);
+        if(file.files[0].name === "records.csv") {
+            upload_button.disabled = false;
+        } else {
+
+        }
+    }
+
+    form.onsubmit = function(event) {
+        event.preventDefault();
+
+        upload_button.innerHTML = 'Uploading..';
+
+        var files = file.files;
+        var form_data = new FormData();
+
+        form_data.append('records', files[0], files[0].name);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', BASE_URL + 'upload_records', true);
+        xhr.onload = function() {
+            if(xhr.status === 200) {
+                upload_button.innerHTML = 'Upload Complete';
+            } else {
+                alert("Error Occured in Upload");
+            }
+        };
+
+        xhr.send(form_data);
+    }
+}
