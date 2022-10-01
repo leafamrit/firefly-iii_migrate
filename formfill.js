6572060e3ca8cac8d476581d49a8f90f4c304152
Pratyay Amrit <arkeyve@gmail.com>
2022-10-01 23:46:22 +0530

    Update formfill.js

--- formfill.js
+++ formfill.js
@@ -1,18 +1,32 @@
+// javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://raw.githubusercontent.com/Arkeyve/firefly-iii_migrate/master/formfill.js';})();
+
 let inputstr = prompt("input");
 inputstr = inputstr.split("\t");
 
-document.querySelector("input[name='date[]']").value = inputstr[0].split("/").reverse().join("-");
-document.querySelector("input[name='description[]']").value = inputstr[2];
-document.querySelector("input[name='amount[]']").value = inputstr[3] !== "0.0" ? inputstr[3] : inputstr[4];
-
 let category = "Misc.";
-if(inputstr[2].indexOf("zepto") >= 0) category = "Groceries";
-if(inputstr[2].indexOf("paytm") >= 0) category = "Snacks";
-if(inputstr[2].indexOf("swiggy") >= 0) category = "Food";
-if(inputstr[2].indexOf("DELOITTE") >= 0) category = "Income";
-if(inputstr[2].indexOf("parvatkarmukesh") >= 0) category = "Polo GT";
-if(inputstr[2].indexOf("CHANDRASEK") >= 0) category = "Samarth Garden C/305";
-if(inputstr[2].indexOf("decathlon") >= 0) category = "Shopping";
+let description = "Misc.";
+if(inputstr[2].indexOf("zepto") >= 0) { category = "Groceries"; description = "Zepto"; }
+if(inputstr[2].indexOf("paytm") >= 0) { category = "Snacks"; description = "Paytm"; }
+if(inputstr[2].indexOf("swiggy") >= 0) { category = "Food"; description = "Swiggy"; }
+if(inputstr[2].indexOf("DELOITTE") >= 0) { category = "Income"; description = "Deloitte"; }
+if(inputstr[2].indexOf("parvatkarmukesh") >= 0) { category = "Polo GT"; description = "Car Cleaning"; }
+if(inputstr[2].indexOf("CHANDRASEK") >= 0) { category = "Samarth Garden C/305"; description = "Rent"; }
+if(inputstr[2].indexOf("decathlon") >= 0) { category = "Shopping"; description = "Decathlon"; }
+
+document.querySelector("input[name='date[]']").value = inputstr[0].split("/").reverse().join("-");
+document.querySelector("input[name='description[]']").value = description;
+document.querySelector("input[name='amount[]']").value = inputstr[3] !== "0.0" ? Number(inputstr[3]) : Number(inputstr[4]);
 
 document.querySelector("input[name='category[]']").value = category;
-document.querySelector("input[name='create_another']").checked = true;
+document.querySelector("input[name='create_another']").checked ? undefined : document.querySelector("input[name='create_another']").click();
+
+if ("createEvent" in document) {
+    var evt = document.createEvent("HTMLEvents");
+    evt.initEvent("change", false, true);
+    document.querySelector("input[name='description[]']").dispatchEvent(evt);
+    document.querySelector("input[name='amount[]']").dispatchEvent(evt);
+}
+else {
+    document.querySelector("input[name='description[]']").fireEvent("onchange");
+    document.querySelector("input[name='amount[]']").fireEvent("onchange");
+}
